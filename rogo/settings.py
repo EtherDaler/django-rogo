bfda4c33ad58beb92a1da0916286fe493c7e45ff
EtherDaler <daler.hojimatov@yandex.ru>
2020-06-12 18:00:15 +0500

    fives commit

--- rogo/settings.py
+++ rogo/settings.py
@@ -148,6 +148,7 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     ]
