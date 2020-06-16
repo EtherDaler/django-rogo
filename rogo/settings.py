2ad45e700572c343c34815b5704426486d2eb94e
EtherDaler <daler.hojimatov@yandex.ru>
2020-06-16 23:18:44 +0500

    34 commit

--- rogo/settings.py
+++ rogo/settings.py
@@ -134,7 +134,7 @@ TIME_ZONE = 'Asia/Tashkent'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'Rogofounder@gmail.com'
 EMAIL_HOST_PASSWORD = 'Bulochka1989 '
