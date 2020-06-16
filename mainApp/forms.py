16a61316695a2ab23bb5f7e582942a6fc4afc4b6
EtherDaler <daler.hojimatov@yandex.ru>
2020-06-17 01:36:57 +0500

    39 commit

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import UserModel
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
+from django.utils.translation import gettext as _
+from django.contrib.auth.forms import UserCreationForm
 
 
 class UserForm(ModelForm):
@@ -10,23 +12,14 @@ class UserForm(ModelForm):
         fields = ('phone', 'user_car', 'user_car_year', 'user_car_number')
         exclude = ['user']
 
-class CreateUserForm(forms.ModelForm):
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
-
+class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ['username', 'email', 'password1', 'password2']
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password1'] != cd['password2']:
-            raise forms.ValidationError('Пароли не совпадают.')
-        return cd['password2']
-    
-    def cleaned_email(self):
+    def clean_email(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(username=username).exists():
-            raise forms.ValidationError(u'Такой Электронный адрес уже используется.')
+        if User.objects.filter(email=email).exclude(username=username).exists():
+            raise ValidationError(_('This email already exists'))
         return email
