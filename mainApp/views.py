79118624e7944f4261afc6a04ddaaec1b92958f0
EtherDaler <daler.hojimatov@yandex.ru>
2020-06-16 15:58:49 +0500

    31 commit

--- mainApp/views.py
+++ mainApp/views.py
@@ -49,66 +49,66 @@ def logout_page(request):
     return redirect('index')
 
     
-#def register(request):
-#    if request.method == 'POST':
-#        user_form = UserRegistrationForm(request.POST)
-#        if user_form.is_valid():
-#            # Create a new user object but avoid saving it yet
-#            new_user = user_form.save(commit=False)
-#            # Set the chosen password
-#            new_user.set_password(user_form.cleaned_data['password'])
-#            # Save the User object
-#            user_inf = user_form.cleaned_data
-#            new_user.save()
-#            return render(request, 'registration/signup_done.html', {'new_user': new_user})
-#    else:
-#        user_form = UserRegistrationForm()
-#    return render(request, 'registration/signup.html', {'user_form': user_form})
-
-def register_page(request):
-    form = CreateUserForm()
+def register(request):
     if request.method == 'POST':
-        form = CreateUserForm(request.POST)
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            user_inf = user_form.cleaned_data
+            new_user.save()
+            return render(request, 'registration/signup_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'registration/signup.html', {'user_form': user_form})
 
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-            current_site = get_current_site(request)
-            mail_subject = 'Подтвердите свой email.'
-            message = render_to_string('registration/acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user)
-            })
-            toemail = form.cleaned_data.get('email')
-            email = EmailMessage(mail_subject, message, to=[toemail])
-            username = form.cleaned_data.get('username')
-            email.send()
-
-            return render(request, 'registration/signup_done.html')
+#def register_page(request):
+#    form = CreateUserForm()
+#    if request.method == 'POST':
+#        form = CreateUserForm(request.POST)
+#
+#        if form.is_valid():
+#            user = form.save(commit=False)
+#            user.is_active = False
+#            user.save()
+#            current_site = get_current_site(request)
+#            mail_subject = 'Подтвердите свой email.'
+#            message = render_to_string('registration/acc_active_email.html', {
+#                'user': user,
+#                'domain': current_site.domain,
+#                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+#                'token': account_activation_token.make_token(user)
+#            })
+#            toemail = form.cleaned_data.get('email')
+#            email = EmailMessage(mail_subject, message, to=[toemail])
+#            username = form.cleaned_data.get('username')
+#            email.send()
+#
+#            return render(request, 'registration/signup_done.html')
 
             #messages.success(request, "Account was created for " + username)
             #return redirect('login')
+#
+#    context = {
+#        'form': form
+#    }
+#    return render(request, 'registration/signup.html', context)
 
-    context = {
-        'form': form
-    }
-    return render(request, 'registration/signup.html', context)
 
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        return redirect('home')
-        #return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
-    else:
-        return HttpResponse('Activation link is invalid!')
+#def activate(request, uidb64, token):
+#    try:
+#        uid = force_text(urlsafe_base64_decode(uidb64))
+#        user = User.objects.get(pk=uid)
+#    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+#        user = None
+#    if user is not None and account_activation_token.check_token(user, token):
+#        user.is_active = True
+#        user.save()
+#        login(request, user)
+#        return redirect('home')
+#        #return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+#    else:
+#        return HttpResponse('Activation link is invalid!')
