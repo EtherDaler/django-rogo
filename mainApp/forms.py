25c90b2d3c0e0e11d84f9725d3cd7fb7759b58a7
EtherDaler <daler.hojimatov@yandex.ru>
2020-06-16 19:04:04 +0500

    31 commit

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import UserModel
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 
 
@@ -10,23 +11,14 @@ class UserForm(ModelForm):
         fields = ('phone', 'user_car', 'user_car_year', 'user_car_number')
         exclude = ['user']
 
-class CreateUserForm(forms.ModelForm):
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
-
+class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ['username', 'first_name', 'last_name','email', 'password1', 'password2']
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Пароли не совпадают.')
-        return cd['password2']
-    
-    def cleaned_email(self):
+    def clean_email(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(username=username).exists():
-            raise forms.ValidationError(u'Такой Электронный адрес уже используется.')
+        if User.objects.filter(email=email).exclude(username=username).exists():
+            raise ValidationError(_('This email already exists'))
         return email
--- mainApp/urls.py
+++ mainApp/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path('', views.index, name="index"),
 
 
-    path('register/', views.register, name="register"),
+    path('register/', views.register_page, name="register"),
     path('login/', views.login_page, name='login'),
     path('logout/',views.logout_page,name='logout'),
     path('home/',views.home,name='home'),
-#    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 
     path('reset_password/', auth_view.PasswordResetView.as_view(template_name='registration/password_reset.html'),
          name='reset_password'),
